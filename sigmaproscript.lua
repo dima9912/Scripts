9bf2b7ce49a4103545cdcebcf077f7a4aab331e8
Sk3d.Yinzol <100521144+dima9912@users.noreply.github.com>
2026-03-07 12:47:50 +0500

    Update sigmaproscript.lua

--- sigmaproscript.lua
+++ sigmaproscript.lua
@@ -85,7 +85,7 @@ end)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/krissisis898-dev/Roblox-/refs/heads/main/Roblox%20Anti%20AFK-Script%20(updatet)", true))()
 
-wait(20)
+wait(5)
 if TextLabel2.Text == "Брейнрот: " then
 		game.Players.LocalPlayer:Kick("пш пш по приколу")
 		wait()
